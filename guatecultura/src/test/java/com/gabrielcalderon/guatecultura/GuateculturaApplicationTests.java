ebc53ffb08d63e91984ab6b30dbe7fce4d5cd0e0
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-06-17 10:30:18 -0600

    feat: Se agrego el proyecto raíz de spring boot

---
+++ guatecultura/src/test/java/com/gabrielcalderon/guatecultura/GuateculturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielcalderon.guatecultura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuateculturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
