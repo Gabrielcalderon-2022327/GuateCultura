854d0a0848d40841f4c95c1bea066065beb817e6
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-01 07:50:21 -0600

    feat: Se agrego interfaz para la entidad User

---
+++ guatecultura-ts/src/models/entity/User.ts
@@ -0,0 +1,13 @@
+import { UserRole } from "../enums/UserRole";
+
+export interface User {
+    user_id: number;
+    nombre_usuario: string;
+    email: string;
+    password_hash: string;
+    first_name: string;
+    last_name: string;
+    rol: UserRole;
+    created_at?: Date;
+    updated_at?: Date;
+}
