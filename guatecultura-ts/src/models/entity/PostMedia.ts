b3c3f6e54454204bf3371d187def5f47604a4ff4
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-01 07:58:19 -0600

    feat: Se agrego interfaz para la entidad PostMedia

---
+++ guatecultura-ts/src/models/entity/PostMedia.ts
@@ -0,0 +1,8 @@
+import { MediaType } from "../enums/MediaType";
+
+export interface PostMedia {
+    media_id: number;
+    FK_post_id: number;
+    media_url?: string | null;
+    media_type?: MediaType | null;
+}
