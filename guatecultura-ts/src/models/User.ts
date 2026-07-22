807db272cf44f189d5e672a720304572baa63d6e
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-22 07:51:50 -0600

    feat: Se cambio un atributo de user

--- guatecultura-ts/src/models/User.ts
+++ guatecultura-ts/src/models/User.ts
@@ -4,7 +4,7 @@ export interface User {
     user_id: number;
     nombre_usuario: string;
     email: string;
-    password_hash: string;
+    password: string;
     first_name: string;
     last_name: string;
     rol: UserRole;
