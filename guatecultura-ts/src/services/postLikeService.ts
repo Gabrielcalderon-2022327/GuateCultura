086d473c63d2b1225fe1375a03cfde3e9d8d4f7c
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-24 14:05:18 -0600

    feat: Se actualizaron el service y el validator de PostLike para implementar la conexión con la DB

--- guatecultura-ts/src/services/postLikeService.ts
+++ guatecultura-ts/src/services/postLikeService.ts
@@ -1,33 +1,62 @@
+import { RowDataPacket, ResultSetHeader } from "mysql2";
+import pool from "../config/db";
 import { PostLike } from "../models/PostLike";
+import { NotFoundException } from "../exceptions/notFoundException";
+import { validatePostLike } from "../validators/postLikeValidator";
 
-const postLikes: PostLike[] = [];
-
-export function getAllPostLikes(): PostLike[] {
-    return postLikes;
+export async function getAllPostLikes(): Promise<PostLike[]> {
+    const [rows] = await pool.query<RowDataPacket[]>("select * from PostLikes");
+    return rows as PostLike[];
 }
 
-export function getPostLikeById(id: number): PostLike | undefined {
-    return postLikes.find(pl => pl.post_like_id === id);
-}
+export async function getPostLikeById(id: number): Promise<PostLike> {
+    const [rows] = await pool.query<RowDataPacket[]>(
+        "select * from PostLikes where post_like_id = ?",
+        [id]
+    );
 
-export function createPostLike(postLike: PostLike): void {
-    postLikes.push(postLike);
-}
-
-export function editPostLike(id: number, postLike: PostLike): boolean {
-    const postLikeIndex = postLikes.findIndex(pl => pl.post_like_id === id);
-    if (postLikeIndex === -1) {
-        return false;
+    if (rows.length === 0) {
+        throw new NotFoundException(`Like de post con id ${id} no encontrado`);
     }
-    postLikes[postLikeIndex] = { ...postLike, post_like_id: id };
-    return true;
+
+    return rows[0] as PostLike;
 }
 
-export function deletePostLike(id: number): boolean {
-    const postLikeIndex = postLikes.findIndex(pl => pl.post_like_id === id);
-    if (postLikeIndex === -1) {
-        return false;
-    }
-    postLikes.splice(postLikeIndex, 1);
-    return true;
+export async function createPostLike(postLike: PostLike): Promise<PostLike> {
+    await validatePostLike(postLike);
+
+    const [result] = await pool.query<ResultSetHeader>(
+        `insert into PostLikes (FK_user_id, FK_post_id)
+            VALUES (?, ?)`,
+        [
+            postLike.FK_user_id,
+            postLike.FK_post_id
+        ]
+    );
+
+    return await getPostLikeById(result.insertId);
+}
+
+export async function editPostLike(id: number, postLike: PostLike): Promise<PostLike> {
+    await getPostLikeById(id);
+    await validatePostLike(postLike);
+
+    await pool.query(
+        `update PostLikes
+            set FK_user_id = ?, FK_post_id = ?
+        where post_like_id = ?`,
+        [
+            postLike.FK_user_id,
+            postLike.FK_post_id,
+            id
+        ]
+    );
+
+    return await getPostLikeById(id);
+}
+
+export async function deletePostLike(id: number): Promise<void> {
+    await getPostLikeById(id);
+
+    await pool.query("delete from PostLikes where post_like_id = ?", [id]);
 }
--- guatecultura-ts/src/validators/postLikeValidator.ts
+++ guatecultura-ts/src/validators/postLikeValidator.ts
@@ -7,12 +7,12 @@ const requiredPostLikeFields: (keyof PostLike)[] = [
     "FK_user_id", "FK_post_id"
 ];
 
-export function validatePostLike(postLike: PostLike): void {
+export async function validatePostLike(postLike: PostLike): Promise<void>{
     validateRequiredFields(postLike, requiredPostLikeFields);
 
     // FKs EXISTENTES
-    getUserById(postLike.FK_user_id);
-    getPostById(postLike.FK_post_id);
+    await getUserById(postLike.FK_user_id);
+    await getPostById(postLike.FK_post_id);
 
     // validación pendiente: unicidad compuesta (FK_user_id + FK_post_id), error DB
 }
