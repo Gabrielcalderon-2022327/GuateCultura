7aef1931c56e5e62cca42977f0c5c8d6f3e47e84
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-23 19:10:05 -0600

    feat: Se agregaron mas validaciones

---
+++ guatecultura-ts/src/validators/paymentValidator.ts
@@ -0,0 +1,15 @@
+import { Payment } from "../models/Payment";
+import { PaymentStatus } from "../models/enums/PaymentStatus";
+import {validateRequiredFields,validateEnum,validatePositiveNumber} from "./validators";
+import { getUserById } from "../services/userService";
+
+const requiredPaymentFields: (keyof Payment)[] = [
+    "amount", "status", "FK_user_id"
+];
+
+export function validatePayment(payment: Payment): void {
+    validateRequiredFields(payment, requiredPaymentFields);
+    validatePositiveNumber(payment.amount, "amount");
+    validateEnum(payment.status, PaymentStatus, "status");
+    getUserById(payment.FK_user_id);
+}
