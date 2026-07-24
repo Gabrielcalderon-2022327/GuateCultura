f8f9f4e2e0fa2a314399cb0eba69387c810bb772
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-23 19:09:49 -0600

    feat: Se agregaron las validaciones para los pagos

--- guatecultura-ts/src/models/Payment.ts
+++ guatecultura-ts/src/models/Payment.ts
@@ -3,7 +3,7 @@ import { PaymentStatus } from "./enums/PaymentStatus";
 export interface Payment {
     payment_id: number;
     amount: number;
-    FK_user_id?: number | null;
+    FK_user_id: number;
     status: PaymentStatus;
     created_at?: Date;
 }
