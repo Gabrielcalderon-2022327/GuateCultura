8fa2012b58994fdcd171fe000c1c84eaf74a85ed
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-24 14:06:48 -0600

    feat: Se actualizaron el service y el validator de ProductionComment para implementar la conexión con la DB

--- guatecultura-ts/src/validators/productionCommentValidator.ts
+++ guatecultura-ts/src/validators/productionCommentValidator.ts
@@ -7,12 +7,12 @@ const requiredProductionCommentFields: (keyof ProductionComment)[] = [
     "content", "FK_user_id", "FK_production_id"
 ];
 
-export function validateProductionComment(productionComment: ProductionComment): void {
+export async function validateProductionComment(productionComment: ProductionComment): Promise<void> {
     validateRequiredFields(productionComment, requiredProductionCommentFields);
     validateMinLength(productionComment.content, 1, "content");
     validateMaxLength(productionComment.content, 1000, "content");
 
     // FKs EXISTENTES
-    getUserById(productionComment.FK_user_id);
-    getProductionById(productionComment.FK_production_id);
+    await getUserById(productionComment.FK_user_id);
+    await getProductionById(productionComment.FK_production_id);
 }
